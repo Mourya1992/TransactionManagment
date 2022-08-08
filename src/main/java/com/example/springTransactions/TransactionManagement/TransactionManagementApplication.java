3cda15efc2ad95ce055bd9e842833e4988c6176d
Mourya <themourya.v@yahoo.com>
2022-08-08 23:27:59 +0530

    initial commit

---
+++ src/main/java/com/example/springTransactions/TransactionManagement/TransactionManagementApplication.java
@@ -0,0 +1,16 @@
+package com.example.springTransactions.TransactionManagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class
+TransactionManagementApplication {
+
+    public static void main(String[] args) {
+
+        SpringApplication.run(TransactionManagementApplication.class, args);
+        System.out.println("Say Hi");
+    }
+
+}
---
+++ src/test/java/com/example/springTransactions/TransactionManagement/TransactionManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springTransactions.TransactionManagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TransactionManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
